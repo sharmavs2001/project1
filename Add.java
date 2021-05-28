e6022a4e8dd6f62220d58518df8c7f0f470071ab
sharmavs2001 <sharmavs2001@gmail.com>
2021-05-28 20:37:18 +0530

    First commit

---
+++ Add.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Scanner sc= new Scanner(System.in);
+ System.out.println("Addition - Enter 2 numbers!");
+ int a= sc.nextInt();
+ int b= sc.nextInt();
+
+ System.out.println("Addition = " + (a+b));
+ 
+	}
+
+}
+

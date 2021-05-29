42e3c2fbe4dd5412ec0124c460640c51d9a53573
sharmavs2001 <sharmavs2001@gmail.com>
2021-05-29 12:33:40 +0530

    NEW BRANCHING COMMIT

---
+++ Substraction.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+
+		
+		 Scanner sc= new Scanner(System.in);
+		 System.out.println("Substraction - Enter 2 numbers!");
+		 int a= sc.nextInt();
+		 int b= sc.nextInt();
+
+		 System.out.println( " Substraction = " + ( a - b ) );
+		 
+		
+
+
+	}
+
+}
